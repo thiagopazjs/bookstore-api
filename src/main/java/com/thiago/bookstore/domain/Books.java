71cb0722eba2f3dc928399fb83ee259ce4756a80
Thiago Paz <thiagopaz907@gmail.com>
2022-01-26 11:39:00 -0300

    finalizando classes de dominio

--- src/main/java/com/thiago/bookstore/domain/Books.java
+++ src/main/java/com/thiago/bookstore/domain/Books.java
@@ -1,12 +1,103 @@
 package com.thiago.bookstore.domain;
 
-public class Books {
+import java.io.Serializable;
+import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Books  implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String titulo;
 	private String nome_autor;
 	private String texto;
-	
+
+	@ManyToOne
+	@JoinColumn(name = "category_id")
 	private Category category;
+
+	public Books() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Books(Integer id, String titulo, String nome_autor, String texto, Category category) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.nome_autor = nome_autor;
+		this.texto = texto;
+		this.category = category;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getNome_autor() {
+		return nome_autor;
+	}
+
+	public void setNome_autor(String nome_autor) {
+		this.nome_autor = nome_autor;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Books other = (Books) obj;
+		return Objects.equals(id, other.id);
+	}
 
 }
--- src/main/java/com/thiago/bookstore/domain/Category.java
+++ src/main/java/com/thiago/bookstore/domain/Category.java
@@ -1,19 +1,91 @@
 package com.thiago.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-public class Category {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+
+@Entity
+public class Category implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	private String description;
 	
+	@OneToMany(mappedBy= "category")
 	private List<Books> books = new ArrayList<>();
 
 	public Category() {
 		super();
 		// TODO Auto-generated constructor stub
+	}
+
+	public Category(Integer id, String name, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Books> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<Books> books) {
+		this.books = books;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
 	}
 	
 	
